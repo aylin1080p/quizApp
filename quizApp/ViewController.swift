fbb2d3a649fe30f918d00462afd7e654a47987be
aylin gunes <4ylingunes@gmail.com>
2023-02-08 17:33:16 +0100

    Initial Commit

---
+++ quizApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  quizApp
+//
+//  Created by Aylin GÜNEŞ on 8.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
+
+}
+
